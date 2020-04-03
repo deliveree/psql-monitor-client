ce77a070822e184148dbdd4445c1a88da77aa37c
nvpmai <mai.nguyen@deliveree.com>
2020-04-03 18:03:02 +0700

    add params for log

--- src/main.py
+++ src/main.py
@@ -1,21 +1,32 @@
 import logging
+import argparse
 
 from modules.client import Client
 from modules.utils import load_conf
 
 
-def config_log(log_conf):
-    log_path = log_conf["filepath"]
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--loglevel", "-l", type=str, default="ERROR",
+    help="The log level (default: ERROR)"
+)
+parser.add_argument(
+    "--logfile", "-f", type=str,
+    help="The path to logfile (default: None)"
+)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    loglevel = args.loglevel
+    logfile = args.logfile
+
     logging.basicConfig(
-        filename=log_path,
-        level=logging.INFO,
+        filename=logfile,
+        level=loglevel,
         format="%(asctime)s - %(message)s"
     )
 
-
-if __name__ == "__main__":
     conf = load_conf()
-    config_log(conf["log"])
     client = Client(conf)
 
     try:
