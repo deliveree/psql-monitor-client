ed8757c749b9bad9f48a4348b71415dd2e54ff24
Hung Tran <hung.tran@deliveree.com>
2020-04-15 18:38:11 +0700

    fixing exception and connection timeout

--- src/modules/psql_connector.py
+++ src/modules/psql_connector.py
@@ -6,10 +6,10 @@ from async_timeout import timeout
 
 class PSQLConnector():
     def __init__(self, conf):
-        loop = asyncio.get_event_loop()
+        self.loop = asyncio.get_event_loop()
         self.sync_type = conf["sync_type"]
         self.conf = conf
-        self.pool = loop.run_until_complete(self._create_pool())
+        self.pool = self.loop.run_until_complete(self._create_pool())
 
     async def _create_pool(self):
         pool = await create_pool(
@@ -18,8 +18,7 @@ class PSQLConnector():
             password=self.conf["password"],
             host=self.conf.get("host", "localhost"),
             port=self.conf.get("port", 5432),
-            max_inactive_connection_lifetime=0.05,
-            timeout=0.05)
+            max_inactive_connection_lifetime=0.05)
 
         logging.info('Successfully connected with local PSQL')
         return pool
@@ -34,7 +33,7 @@ class PSQLConnector():
                 return await self._select_single_execute(query)
         except InterfaceError as e:
             if "connection already closed" in str(e).lower():
-                self.conn = loop.run_until_complete(self._create_pool())
+                self.pool = self.loop.run_until_complete(self._create_pool())
                 async with timeout(0.05):
                     return await self._select_single_execute(query)
             else:
